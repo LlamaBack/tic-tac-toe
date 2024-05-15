c9a4f151fc6b45f72b275a206d9a4542e5caec2f
Jake Kim <imjakekim@gmail.com>
2024-05-14 22:58:45 -0700

    played around with tailwind grid system and colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <div className="content">
-        <h1>App Component</h1>
-      </div>
+    <div className="grid grid-cols-3 w-96 h-96 gap-4">
+      <button className=''>01</button>
+      <div className='bg-blue-400 focus:bg-blue-700'>01</div>
+      <div className='bg-blue-700'>02</div>
+      <div className='bg-blue-400'>03</div>
+      <div className='bg-blue-700'>04</div>
+      <div className='bg-blue-400'>05</div>
+      <div className='bg-blue-700'>06</div>
+      <div className='bg-blue-400'>07</div>
+      <div className='bg-blue-700'>08</div>
+      <div className='bg-blue-400'>09</div>
     </div>
   )
 }
