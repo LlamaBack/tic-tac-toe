393c64ea1c664b24594f5462ca90b7ee7e8bf898
Jake Kim <imjakekim@gmail.com>
2024-05-23 13:08:44 -0700

    fixed rendering bug. draw still does not work

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,33 +1,69 @@
 import { useState, useEffect } from "react"; //I dont know what this does
 import Board from "./Board";
 import GameState from "./GameState";
-import GameOver
- from "./GameOver";
+import GameOver from "./GameOver";
 const PLAYER_X = "X"
 const PLAYER_O = "O"
 
-const winningCombinations = [
-  [0,1,2], [3,4,5], [6,7,8],
-  [0,3,6], [1,4,7], [2,5,8],
-  [0,4,8], [2,4,6]
-]
+function checkWinner(squares, setGameState, index, playerTurn, setPlayerTurn) {
+  for (let i = 0; i < squares.length; i++){
+    console.log(playerTurn)
+    if( squares[i][index[1]] !== playerTurn) {
+        break;
+    }
+    if (i === squares.length - 1) {
+      if(playerTurn === PLAYER_X) {
+        setGameState(GameState.playerXWins)
+      } else {
+        setGameState(GameState.playerOWins)
+      }
+      return;
+    }
+  }
 
-function checkWinner(squares, setGameState) {
-  for(const winningCombination of winningCombinations){
-    const squareValue1 = squares[winningCombination[0]]
-    const squareValue2 = squares[winningCombination[1]]
-    const squareValue3 = squares[winningCombination[2]]
+  for (let i = 0; i < squares.length; i++){
+    if( squares[index[0]][i] !== playerTurn) {
+        break;
+    }
+    if (i === squares.length - 1) {
+      if(playerTurn === PLAYER_X) {
+        setGameState(GameState.playerXWins)
+      } else {
+        setGameState(GameState.playerOWins)
+      }
+      return;
+    }
+  }
 
-    if(
-      squareValue1 !== null && 
-      squareValue1 === squareValue2 && 
-      squareValue2 === squareValue3 ) {
-        if(squareValue1 === PLAYER_X) {
+  if (index[1] === index[0]) {
+    for (let i = 0; i < squares.length; i++){
+      if( squares[i][i] !== playerTurn) {
+          break;
+      }
+      if (i === squares.length - 1) {
+        if(playerTurn === PLAYER_X) {
           setGameState(GameState.playerXWins)
         } else {
           setGameState(GameState.playerOWins)
         }
         return;
+      }
+    }
+  }
+
+  if (index[1] + index[0] === squares.length - 1) {
+    for (let i = 0; i < squares.length; i++){
+      if( squares[i][(squares.length - 1) - i] !== playerTurn) {
+          break;
+      }
+      if (i === squares.length - 1) {
+        if(playerTurn === PLAYER_X) {
+          setGameState(GameState.playerXWins)
+        } else {
+          setGameState(GameState.playerOWins)
+        }
+        return;
+      }
     }
   }
 
@@ -35,41 +71,41 @@ function checkWinner(squares, setGameState) {
   if (isDraw) {
     setGameState(GameState.draw);
   }
-}
+
+  if (playerTurn === PLAYER_X) {
+    setPlayerTurn(PLAYER_O);
+  } else {
+    setPlayerTurn(PLAYER_X);
+  }
+
+};
 
 const TicTacToe = () => {
   const [squares, setSquares] = useState(Array(3).fill(null).map(() => Array(3).fill(null)));
   //setSquares > rebinds squares to new input. i assume it must be of the same parameter type?
 
-  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_O);
   const [gameState, setGameState] = useState(GameState.inProgress);
+  const [coord, setCoord] = useState([0, 0]);
 
   const handleSquareClick = (index) => {
-    // console.log(squares[index[0]][index[1]])
-
-
-    // if (gameState !== GameState.inProgress) {
-    //   return;
-    // }
+    setCoord(index);
+    if (gameState !== GameState.inProgress) {
+      return;
+    }
 
     if(squares[index[0]][index[1]] !== null) {
       return;
     }
     const newSquares = JSON.parse(JSON.stringify(squares));
     newSquares[index[0]][index[1]] = playerTurn;
-    // console.log(newSquares[index[0]][index[1]])
-
-    setSquares(newSquares)
-    if (playerTurn === PLAYER_X) {
-      setPlayerTurn(PLAYER_O);
-    } else {
-      setPlayerTurn(PLAYER_X)
-    }
+    setSquares(newSquares);
   };
 
   useEffect(() => {
-    checkWinner(squares, setGameState);
+    checkWinner(squares, setGameState, coord, playerTurn, setPlayerTurn);
   }, [squares]);
+
 
   return (
     <div>
