b2a56d1899d7cdcfbc6d7982792b939276535813
Jake Kim <imjakekim@gmail.com>
2024-05-29 17:24:14 -0700

    finally got columns to center and to wrap around squares. but dont really understand why this is the solution that worked

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,18 +3,21 @@ import Strike from "./Strike";
 
 function Board( {squares, onSquareClick, playerTurn} ) {
   return ( 
-    <div className={`grid gap-3 grid-cols-${squares.length} grid-rows-${squares.length} place-items-center`}>
-      {squares.map((rows, x) => (
-        rows.map((square, y) => (
-          <Square playerTurn={playerTurn} 
-          onClick={()=> onSquareClick([x,y])} 
-          value={squares[x][y]} 
-          className="bg-blue-600 w-20"/>
+    //have no idea why this first div works. will need to research asd
+    <div className="flex flex-col items-center"> 
+      <div className={`grid gap-3 grid-cols-${squares.length} grid-rows-${squares.length} place-items-center`}>
+        {squares.map((rows, x) => (
+          rows.map((square, y) => (
+            <Square playerTurn={playerTurn} 
+            onClick={()=> onSquareClick([x,y])} 
+            value={squares[x][y]} 
+            className="bg-blue-600 w-20"/>
+          ))
         ))
-      ))
-      }
+        }
 
 
+      </div>
     </div>
    );
 }
